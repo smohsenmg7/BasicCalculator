00e853a853c65e162a45d62720e0dcf073514bf5
fatememgh <42394127+fatememgh@users.noreply.github.com>
2018-08-24 16:40:12 +0430

    designing UI and initialize elements

--- app/src/main/java/com/example/cs_group/basiccalculato/MainActivity.java
+++ app/src/main/java/com/example/cs_group/basiccalculato/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.cs_group.basiccalculato;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0,btPoint,btClear;
+    Button btAddiction,btSubtraction,btMultiplication,btDivision,btEqual;
 
     /**
      * @fMaghami
@@ -31,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.editText);
+        bt0= findViewById(R.id.button0);
+        bt1= findViewById(R.id.button1);
+        bt2= findViewById(R.id.button2);
+        bt3= findViewById(R.id.button3);
+        bt4= findViewById(R.id.button4);
+        bt5= findViewById(R.id.button5);
+        bt6= findViewById(R.id.button6);
+        bt7= findViewById(R.id.button7);
+        bt8= findViewById(R.id.button8);
+        bt9= findViewById(R.id.button9);
+        btEqual= findViewById(R.id.buttonEqual);
+        btClear= findViewById(R.id.buttonClear);
+        btPoint= findViewById(R.id.buttonPoint);
+        btDivision= findViewById(R.id.buttonDivision);
+        btAddiction= findViewById(R.id.buttonAddiction);
+        btSubtraction= findViewById(R.id.buttonSubtraction);
+        btMultiplication= findViewById(R.id.buttonMultiplication);
         /**
          * @fMaghami
          * Initialize each view of activity_main.xml like buttons and editText
